e19a87856a36f3f2cd5c62c575aabfcaae575345
TanveerS24 <stanveer1809@gmail.com>
2025-09-27 00:41:18 +0530

    Changed Timer background and font style

--- Client/src/Components/Home.jsx
+++ Client/src/Components/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div className="flex items-center justify-center pt-3 font-medium text-[#006064] text-5xl 
-    underline">
+    underline italic font-tahoma" style={{fontFamily:"Verdana, sans-serif"}}>
       To-Do List
     </div>
   )
--- Client/src/Components/Timer.jsx
+++ Client/src/Components/Timer.jsx
@@ -11,12 +11,14 @@ const Timer = () => {
         return () => clearInterval(timer);
     }, [])
   return (
-    <>
-        <div className='absolute bottom-1 left-1 text-[#006064]'>
-            {time.toLocaleTimeString()}
-        </div>
-    </>
-  )
+  <>
+    <div className='absolute bottom-2.5 left-2.5 text-[#006064] bg-[#99e8f2] rounded-full px-4 py-2 shadow-md hover:scale-105
+        transition-all ease-out duration-150 hover:text-[#025154] hover:bg-[#7fe7f5]'>
+      {time.toLocaleTimeString([], {hour:"2-digit", minute:"2-digit"})}
+    </div>
+  </>
+)
+
 }
 
 export default Timer
