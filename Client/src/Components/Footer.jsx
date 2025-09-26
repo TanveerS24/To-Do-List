a215ad1bb391991084525c7798a4ba1cfb6f2c78
TanveerS24 <stanveer1809@gmail.com>
2025-09-26 13:45:26 +0530

    External links in footer

--- Client/src/Components/Footer.jsx
+++ Client/src/Components/Footer.jsx
@@ -8,16 +8,22 @@ const Footer = () => {
         <div className='bg-cyan-800 p-4'>
             <div className='grid grid-cols-3'>
                 <div className='flex items-center justify-center'>
-                    <FaLinkedin size={30}/>
-                    <a href=""></a>
+                    <a href="https://www.linkedin.com/in/s-tanveer-muhammed-611b89336/" 
+                        target='_blank' rel='noopener norefferer'>
+                        <FaLinkedin size={30}/>
+                    </a>
                 </div>
                 <div className='flex items-center justify-center'>
-                    <FaGithub size={30}/>
-                    <a href=""></a>
+                     <a href="https://github.com/TanveerS24" 
+                        target='_blank' rel='noopener norefferer'>
+                        <FaGithub size={30}/>
+                    </a>
                 </div>
                 <div className='flex items-center justify-center'>
-                    <FaInstagram size={30}/>
-                    <a href=""></a>
+                    <a href="https://www.instagram.com/epicblue06/" 
+                        target='_blank' rel='noopener norefferer'>
+                        <FaInstagram size={30}/>
+                    </a>
                 </div>
             </div>
         </div>
