5e317ca80b5761289b6c878c8423eb9266c8732d
TanveerS24 <stanveer1809@gmail.com>
2025-09-27 22:15:54 +0530

    New Record

---
+++ Client/src/Components/NewRecord.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewRecord = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default NewRecord
--- Client/src/Components/RecordCard.jsx
+++ Client/src/Components/RecordCard.jsx
@@ -3,8 +3,7 @@ import React from 'react'
 const RecordCard = ({record}) => {
   return (
     <div>
-      {record.title}
-      {record.completed.toString()}
+      {record.title} - {record.completed.toString()}
     </div>
   )
 }
